53a091e88557e0544e91ba204e009271b0f6623a
Daniele Martinoli <dmartino@redhat.com>
2022-11-03 15:32:45 +0100

    Fixed startup script issue and renamed artifact

--- kafka/startLoader.sh
+++ kafka/startLoader.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 source .env
 
-KAFKA_HOST=$(grep kafkaHost ./persons.json | cut -d ":" -f2 | tr -d '", ')
-RHOAS_SERVICE_ACCOUNT_CLIENT_ID=$(grep clientID ./sa-credentials.json | cut -d ":" -f2 | tr -d '", ')
-RHOAS_SERVICE_ACCOUNT_CLIENT_SECRET=$(grep clientSecret ./sa-credentials.json | cut -d ":" -f2 | tr -d '", ')
-RHOAS_SERVICE_ACCOUNT_OAUTH_TOKEN_URL=https://sso.redhat.com/auth/realms/redhat-external/protocol/openid-connect/token
+export KAFKA_HOST=$(grep kafkaHost ./persons.json | cut -d ":" -f2 | tr -d '", ')
+export RHOAS_SERVICE_ACCOUNT_CLIENT_ID=$(grep clientID ./sa-credentials.json | cut -d ":" -f2 | tr -d '", ')
+export RHOAS_SERVICE_ACCOUNT_CLIENT_SECRET=$(grep clientSecret ./sa-credentials.json | cut -d ":" -f2 | tr -d '", ')
+export RHOAS_SERVICE_ACCOUNT_OAUTH_TOKEN_URL=https://sso.redhat.com/auth/realms/redhat-external/protocol/openid-connect/token
 
 echo "Starting Quarkus loader with"
 echo "$KAFKA_HOST"
